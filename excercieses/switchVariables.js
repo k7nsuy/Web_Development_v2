cae31ee5d6f7090c5b7497fb3de24dd7bfca8035
k7nsuy <76999175+k7nsuy@users.noreply.github.com>
2022-08-10 20:34:25 +0900

    11
    11

---
+++ excercieses/switchVariables.js
@@ -0,0 +1,13 @@
+function switchVariables() {
+    var a = 3;
+    var b = 8;
+
+    var c = a;
+    var a = b;
+    var b = c;
+
+    console.log(a);
+    console.log(b);
+}
+
+switchVariables();
---
+++ index.js
@@ -0,0 +1,6 @@
+var a = "3";
+var b = "4";
+
+var a = "6";
+
+console.log(a);
