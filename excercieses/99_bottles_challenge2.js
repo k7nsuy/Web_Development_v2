0d3f3eba9f0ce717d3f0567fd13df728b30860a1
k7nsuy <76999175+k7nsuy@users.noreply.github.com>
2022-08-12 22:54:15 +0900

    44
    44

--- excercieses/99_bottles_challenge2.js
+++ excercieses/99_bottles_challenge2.js
@@ -1,11 +1,17 @@
+
 var numberOfBottles = 99
+
+function bottlesOfWall() {
+
+
 while (numberOfBottles >= 0) {
     var bottleWord = "bottles"
     if ( numberOfBottles === 1) {
         var bottleWord = "bottle"
     } 
-    console.log(`${numberOfBottles} ${bottleWord} of beer on the wall.`)
-    console.log(`${numberOfBottles} ${bottleWord} of beer, Take one down, pass it around.`)
+    console.log(`${numberOfBottles} ${bottleWord} of beer on the wall,
+    ${numberOfBottles} ${bottleWord} of beer, Take one down, pass it around,`)
     numberOfBottles--;
     console.log(`${numberOfBottles} ${bottleWord} of beer on the wall.`)
+    }
 }
