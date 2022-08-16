07dd8fa69447c31df665049820377a85ce52943c
k7nsuy <76999175+k7nsuy@users.noreply.github.com>
2022-08-17 00:40:11 +0900

    33
    33

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -9,16 +9,23 @@ app.get('/', function(req,res) {
 
         response.on("data",function(data) {
             const weatherData = JSON.parse(data)
-            const object = {
-                name: "dd",
-                hobby: "aa"
-            }
-
-            console.log(JSON.stringify(object))
-
             console.log(weatherData)
-        })
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+            res.write(`<img scr=${imageURL} </img>`)
+            res.write(icon)
 
+            res.write(`<h1>The temperature in London is ${temp} degrees Celcius.</h1>`)
+            // const object = {
+            //     name: "dd",
+            //     hobby: "aa"
+            // }
+
+            // console.log(JSON.stringify(object))
+
+        })
         console.log(response.statusCode);
     })
 })  
