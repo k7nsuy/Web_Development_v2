c44ba7d7d8dcf6f8430a3ede672d384848d572eb
k7nsuy <76999175+k7nsuy@users.noreply.github.com>
2022-08-22 13:00:31 +0900

    33
    33

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -1,35 +1,46 @@
 const express = require('express');
 const https = require("https")
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req,res) {
-    
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=<API Key>&units=metric"
-    https.get(url, function(response) {
-
-        response.on("data",function(data) {
-            const weatherData = JSON.parse(data)
-            console.log(weatherData)
-            const temp = weatherData.main.temp
-            const weatherDescription = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
-            res.write(`<img scr=${imageURL} </img>`)
-            res.write(icon)
-
-            res.write(`<h1>The temperature in London is ${temp} degrees Celcius.</h1>`)
-            // const object = {
-            //     name: "dd",
-            //     hobby: "aa"
-            // }
-
-            // console.log(JSON.stringify(object))
-
-        })
-        console.log(response.statusCode);
-    })
+    res.sendFile(__dirname + "/index.html" )
 })  
+    // const query = "London"
+    // const apiKey = "" 
+    
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`
+    // https.get(url, function(response) {
 
+    //     response.on("data",function(data) {
+    //         const weatherData = JSON.parse(data)
+    //         console.log(weatherData)
+    //         const temp = weatherData.main.temp
+    //         const weatherDescription = weatherData.weather[0].description
+    //         const icon = weatherData.weather[0].icon
+    //         const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+    //         res.write(`<img scr=${imageURL} </img>`)
+    //         res.write(icon)
+
+    //         res.write(`<h1>The temperature in London is ${temp} degrees Celcius.</h1>`)
+    //         // const object = {
+    //         //     name: "dd",
+    //         //     hobby: "aa"
+    //         // }
+
+    //         // console.log(JSON.stringify(object))
+
+    //     })
+    //     console.log(response.statusCode);
+    // })
+
+
+app.post('/' , function(req,res) {
+    console.log(req.body.cityName); 
+    console.log("Post request recieved");
+})
 
 
 app.listen('3002')
