5872a0d91cb87d09d07e1cd4cf484f471b109378
k7nsuy <76999175+k7nsuy@users.noreply.github.com>
2022-08-23 20:35:27 +0900

    use
    use

--- newslatter-Signup/app.js
+++ newslatter-Signup/app.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser')
 const request = require('request')
 
 const app = express()
+
+app.use(express.static("public"))
+
 app.listen(3003, function() {
     console.log(`Server is running on port 3003`);
+})
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + "/signup.html")
 })
